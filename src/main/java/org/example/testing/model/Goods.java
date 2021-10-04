5e3b135e71c39dc47d5af14a1244144951aeb644
Igor <igorbochkov27@gmail.com>
2021-10-04 20:46:58 +0300

    init

---
+++ src/main/java/org/example/testing/model/Goods.java
@@ -0,0 +1,18 @@
+package org.example.testing.model;
+
+public class Goods {
+
+    String ok;
+
+    Good[] goods;
+
+    class Good {
+        int id;
+        String name;
+        String category;
+        long timestamp;
+        double price;
+
+    }
+
+}
